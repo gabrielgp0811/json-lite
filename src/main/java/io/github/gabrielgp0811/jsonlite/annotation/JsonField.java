5085be376a969c08bf3af1b9a2989cc28e548613
Gabriel Guimarães Pereira <gabrielgp0811@gmail.com>
2025-07-13 00:39:10 -0300

    Add attributes

--- src/main/java/io/github/gabrielgp0811/jsonlite/annotation/JsonField.java
+++ src/main/java/io/github/gabrielgp0811/jsonlite/annotation/JsonField.java
@@ -3,6 +3,7 @@
  */
 package io.github.gabrielgp0811.jsonlite.annotation;
 
+import static java.lang.annotation.ElementType.FIELD;
 import static java.lang.annotation.ElementType.TYPE;
 import static java.lang.annotation.RetentionPolicy.RUNTIME;
 
@@ -15,13 +16,38 @@ import java.lang.annotation.Target;
  * @author gabrielgp0811
  */
 @Retention(RUNTIME)
-@Target(TYPE)
+@Target({ FIELD, TYPE })
 public @interface JsonField {
 
 	/**
 	 * @return The attribute's name.
 	 */
-	String value();
+	String value() default "";
+
+	/**
+	 * @return The attribute's custom name for serialization/deserialization.
+	 */
+	String customName() default "";
+
+	/**
+	 * @return The attribute's custom name for serialization.
+	 */
+	String customNameSerialization() default "";
+
+	/**
+	 * @return The attribute's custom name for deserialization.
+	 */
+	String customNameDeserialization() default "";
+
+	/**
+	 * @return The attribute's custom names for deserialization.
+	 */
+	String[] customNamesDeserialization() default {};
+
+	/**
+	 * @return The serializer's name from child object.
+	 */
+	String serializerName() default "";
 
 	/**
 	 * The date/time pattern, in case attribute is a <code>java.util.Date</code>,
